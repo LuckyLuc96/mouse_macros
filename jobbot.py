0b112c42f5c722c3b61ef894b2fba0a2b6ed8144
LuckyLuc96 <lucsaybibi@gmail.com>
2024-06-12 13:43:33 -0400

    Added classes to follow OOP. Added the ability to extract coordinates from the text that is recorded. Next up is executing the coordinates and having the program click them in a timely manner

--- jobbot.py
+++ jobbot.py
@@ -1,96 +1,88 @@
-from pynput.mouse import Controller as MouseController
-from pynput.keyboard import Controller as KeyboardController
-from pynput import mouse
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyboardListener
 from pynput import keyboard
 import time
 
+class Recorder:
+    def __init__(self):
+        self.recorded_inputs = []
+        self.mouse_listener = MouseListener(on_move=self.on_move, on_click=self.on_click, on_scroll=self.on_scroll)
+        self.keyboard_listener = KeyboardListener(on_release=self.on_release)
+    
+    def on_move(self, x, y):
+        pass
 
-recorded_inputs = []
+    def on_scroll(self, x, y, dx, dy):
+        pass
 
-### From here to the next comment is preamble to utilize pynput mouse and keyboard. See pynput documentaion
-def on_move(x, y):
-    pass
-
-def on_scroll(x, y, dx, dy):
-    pass
-
-def on_click(x, y, button, pressed):
-    if pressed:
-        print(f"Mouse Click: {x}, {y}")
-        recorded_inputs.append(f"Mouse Click: {x}, {y}\n")
-        
-def on_release(key):  
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False    
-####
-
-def save_inputs_to_file():
-    filename = ("saved_mouse_clicks.txt")
-    if filename:
+    def on_click(self, x, y, button, pressed):
+        if pressed:
+            print(f"Mouse Click: {x}, {y}")
+            self.recorded_inputs.append(f"Mouse Click: {x}, {y}\n")
+    
+    def on_release(self, key):  
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+    
+    def save_inputs_to_file(self, filename="saved_mouse_clicks.txt"):
         with open(filename, "w") as file:
-            for input_event in recorded_inputs:
+            for input_event in self.recorded_inputs:
                 file.write(f"{input_event}")
-            file.close()
             
-def read_and_execute_coordinates(file_path):
-    recorded_inputs = []
-    with open(file_path, "r") as file:
-            for contents in file:
-                contents = contents.strip()
-                recorded_inputs.append((contents))
-                print(contents)
-                    
 
-    
-    # def extract_coordinates(recorded_inputs):
-        # for item in recorded_inputs:
-            # parts = recorded_inputs.split(': ')[1].split(', ')
-            # x = int(parts[0])
-            # y = int(parts[1])
-            # return x, y
-        # print("x",x, "y", y)
-    # extract_coordinates(recorded_inputs)
-                
-def process_manager():
-    try:
-        get_step = int(input("Are you 1) recording or 2) playing the recording? \n"))
-    except Exception as e: print("There has been an error. See: ",e)
-    
-    if get_step == 1:
+    def start_recording(self):
         print("Recording of the mouse has started. Press the escape key to stop recording.")
-        while True:
-            try:
-                #Start mouse listener
-                listener = mouse.Listener(
-                on_move=on_move,
-                on_click=on_click,
-                on_scroll=on_scroll)
-                listener.start()
-                
-                #Start keyboard listener. This is so it can know when you hit "esc" to end recording.
-                with keyboard.Listener(
-                on_release=on_release) as keyboard_listener:
-                    keyboard_listener.join()
-                on_release(0) #0 becomes variable "key" in the on_release function. The value "0" doesn't mean anything here.
-            except Exception as e: print("There has been an error. See: ",e)
-            finally:
-                save_inputs_to_file()
-                return False
+        self.mouse_listener.start()
+        with self.keyboard_listener as keyboard_listener:
+            keyboard_listener.join()
+        self.save_inputs_to_file()
+
+class Playback:
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    def read_and_execute_coordinates(self):
+        extracted_coordinates = []
+        with open(self.file_path, "r") as file:
+            contents = file.read().strip()
+            contents = contents.replace(",", " ")
+            contents = contents.split()
+            print(contents) 
+            coordinate = [int(num) for num in contents if num.isdigit()]
             
-    if get_step == 2:
+            for i in range(0, len(contents), 2):
+                if i + 1 < len(contents):
+                    x = contents[i]
+                    y = contents[i + 1]
+                    if x.isdigit() and y.isdigit():
+                        extracted_coordinates.append([x, y])
+                
+            print(extracted_coordinates)
+            print(type(extracted_coordinates))
+                
+    
+class Manager:
+    def __init__(self):
+        self.recorder = Recorder()
+        self.playback = Playback(file_path="saved_mouse_clicks.txt")
+    
+    def process_manager(self):
         try:
-            read_and_execute_coordinates(file_path="saved_mouse_clicks.txt")
-            #This block is a repeat of above step 1. This is to ensure execution of the macro can be stopped by pressing "esc"
-            with keyboard.Listener(                     ##
-            on_release=on_release) as keyboard_listener:##
-                keyboard_listener.join()                ##
-            on_release(0)                               ##
-        except Exception as e: print("There has been an error. See: ",e)
-    else:
-        print("Please input the numbers 1 or 2. To record a series of mouse clicks, press 1. \n To play the recorded mouse clicks, press 2.")
+            get_step = int(input("Are you 1) recording or 2) playing the recording? \n"))
+        except Exception as e:
+            print("There has been an error. See: ", e)
+            return
+        
+        if get_step == 1:
+            self.recorder.start_recording()
+        elif get_step == 2:
+            self.playback.read_and_execute_coordinates()
+        else:
+            print("Please input the numbers 1 or 2. To record a series of mouse clicks, press 1. \n To play the recorded mouse clicks, press 2.")
 
 def main():
-    process_manager()
-    
+    manager = Manager()
+    manager.process_manager()
+
 main()
